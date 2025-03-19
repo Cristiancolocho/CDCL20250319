eeac06b96c74a8cb42c5d3602ff5fb054da9b353
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-03-19 08:55:53 -0600

    Modificando vistas

--- CDCL20250319.AppWebMVC/Models/Brand.cs
+++ CDCL20250319.AppWebMVC/Models/Brand.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CDCL20250319.AppWebMVC.Models;
 
 public partial class Brand
 {
     public int BrandId { get; set; }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
 
     public string BrandName { get; set; } = null!;
+    [Required(ErrorMessage = "Pais es obligatorio.")]
 
     public string? Country { get; set; }
 
